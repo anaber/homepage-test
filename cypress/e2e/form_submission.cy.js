975c746c575fd2916808f4a22cc491e39d2ad03d
Andrew Naber <anaber@gmail.com>
2024-12-11 13:43:20 -0800

    Added Texas page test

--- cypress/e2e/form_submission.cy.js
+++ cypress/e2e/form_submission.cy.js
@@ -19,3 +19,25 @@ describe('Webflow Homepage Form Test', () => {
     cy.contains('So simple').should('be.visible'); // Replace with text or selector unique to the target page
   });
 });
+
+describe('Webflow Texas Form Test', () => {
+  it('fills out the form and submits successfully', () => {
+    // Visit your Webflow website
+    cy.visit('https://www.steadily.com/states/texas');
+
+    // Fill out form fields
+    cy.get('#auto-complete-address').type('400 105th Ave, Oakland, CA 94603');
+
+    // Submit the form
+    cy.get('#wf-form-raw_address > input').click();
+
+    // Validate URL change
+    cy.url().should('include', 'https://quote.steadily.com/dwelling_single_funnel/lead/');
+
+    // Ensure the target page loads correctly (not a 404)
+    cy.get('body').should('not.contain', '404'); // Check for absence of "404" in the body
+
+    // Validate specific content or element on the loaded page
+    cy.contains('So simple').should('be.visible'); // Replace with text or selector unique to the target page
+  });
+});
